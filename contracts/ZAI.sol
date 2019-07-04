8c42b1397cca94a911462ce8b1cc3d6be9a2ab9e
murdakah <32340407+murdakah@users.noreply.github.com>
2019-07-04 18:48:44 +0200

    Update ZAI.sol

--- contracts/ZAI.sol
+++ contracts/ZAI.sol
@@ -4,15 +4,19 @@ import "./StorageConsumer.sol";
 import "./Proxy.sol";
 import "./DetailedToken.sol";
 import "./KeyValueStorage.sol";
+import "./ERC20Burnable.sol";
+import "./ERC20Mintable.sol";
 
-contract ZAI is Migratable, StorageConsumer, Proxy, DetailedToken {
+
+
+contract ZAI is StorageConsumer, Proxy, DetailedToken, ERC20Burnable, ERC20Mintable {
 
     constructor(KeyValueStorage storage_)
         public
         StorageConsumer(storage_)
     {
         // set some immutable state
-        name = "ZAI";
+        name = "ZAI StableCoin for ZAR";
         symbol = "ZAI";
         decimals = 18;
 
