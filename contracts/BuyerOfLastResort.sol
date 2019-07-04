9dd9775570d86abdbce72d8584fa35b9a8eadb54
murdakah <32340407+murdakah@users.noreply.github.com>
2019-07-04 16:27:34 +0200

    Create BuyerOfLastResort.sol

---
+++ contracts/BuyerOfLastResort.sol
@@ -0,0 +1,25 @@
+/*
+Buy and Sell coins
+
+Buy at below market price if no sellers on the network
+
+
+Sell above market price if no buyers on the network
+
+*/
+
+pragma solidity ^0.5.0;
+
+contract BOLR {
+
+    function Buy()    {
+
+
+    }
+
+    function Sell()     {
+
+        
+    }
+
+}
